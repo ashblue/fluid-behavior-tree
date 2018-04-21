306dd400444adfe0b95f35d011a210ff4404dfcc
Ash Blue <ash@blueashes.com>
2018-04-21 00:40:45 -0600

    Logic added for adding nodes to the tree

--- Assets/FluidBehaviorTree/Editor/Testing/BehaviorTree/BehaviorTreeTest.cs
+++ Assets/FluidBehaviorTree/Editor/Testing/BehaviorTree/BehaviorTreeTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Adnc.FluidBT.Trees;
 using FluidBehaviorTree.Scripts.Nodes;
 using NSubstitute;
@@ -6,7 +7,12 @@ using NUnit.Framework;
 
 namespace Adnc.FluidBT.Testing {
     public class BehaviorTreeTests {
-        readonly BehaviorTree tree = new BehaviorTree();
+        BehaviorTree tree;
+        
+        [SetUp]
+        public void SetBehaviorTree () {
+            tree = new BehaviorTree();
+        }
 
         public class OnInit : BehaviorTreeTests {
             [Test]
@@ -20,26 +26,108 @@ namespace Adnc.FluidBT.Testing {
             }
         }
 
+        public class AddNodeMethod : BehaviorTreeTests {
+            private INodeUpdate action;
+
+            [SetUp]
+            public void SetDefaultAction () {
+                action = Substitute.For<INodeUpdate>();
+            }
+
+            public class AddNodeMethodSuccess : AddNodeMethod {
+                [SetUp]
+                public void AddNode () {
+                    tree.AddNode(tree.Root, action);
+                }
+
+                [Test]
+                public void Parent_node_is_added_to_the_child () {
+                    Assert.AreEqual(action, tree.Root.Child);
+                }
+
+                [Test]
+                public void Add_child_node_to_node_list () {
+                    Assert.Contains(action, tree.nodeList);
+                }
+
+                [Test]
+                public void Add_child_node_to_nodes () {
+                    Assert.IsTrue(tree.nodes.Contains(action));
+                }
+            }
+
+            public class AddNodeMethodError : AddNodeMethod {
+                [Test]
+                public void Cannot_add_child_nodes_that_have_already_been_added () {
+                    var actionChild = Substitute.For<INodeRoot>();
+
+                    tree.AddNode(tree.Root, actionChild);
+
+                    Assert.Throws<ArgumentException>(
+                        () => tree.AddNode(actionChild, actionChild),
+                        "Cannot set a child node that has already been added");
+                }
+
+                [Test]
+                public void Cannot_overwrite_an_already_set_child_node () {
+                    var actionChild = Substitute.For<INodeRoot>();
+
+                    tree.AddNode(tree.Root, action);
+
+                    Assert.Throws<ArgumentException>(
+                        () => tree.AddNode(tree.Root, actionChild),
+                        "Cannot overwrite an already set child node");
+                }
+
+                [Test]
+                public void Cannot_add_child_nodes_to_a_parent_not_in_the_bt () {
+                    var actionChild = Substitute.For<INodeRoot>();
+
+                    Assert.Throws<ArgumentException>(
+                        () => tree.AddNode(actionChild, action),
+                        "Cannot add a node to a parent that is not in the BT");
+                }
+
+                [Test]
+                public void Error_if_parent_is_null () {
+                    Assert.Throws<ArgumentNullException>(
+                        () => tree.AddNode(null, action),
+                        "Parent cannot be null");
+                }
+
+                [Test]
+                public void Error_if_child_is_null () {
+                    Assert.Throws<ArgumentNullException>(
+                        () => tree.AddNode(tree.Root, null),
+                        "Child cannot be null");
+                }
+            }
+        }
+
         public class TickMethod : BehaviorTreeTests {
+            private INodeUpdate action;
+
+            [SetUp]
+            public void SetDefaultAction () {
+                action = Substitute.For<INodeUpdate>();
+                action.Update().Returns(NodeStatus.Success);
+                action.Enabled.Returns(true);
+
+                tree.Root.Child = action;
+            }
+
             [Test]
             public void Update_the_current_node_on_tick () {
                 var root = Substitute.For<INodeRoot>();
                 tree.Current = root;
-                tree.Root = root;
 
                 tree.Tick();
 
-                tree.Root.Received().Update();
+                tree.Current.Received().Update();
             }
 
             [Test]
             public void Tick_the_next_action_from_root () {
-                var action = Substitute.For<INodeUpdate>();
-
-                action.Update().Returns(NodeStatus.Success);
-                action.Enabled.Returns(true);
-                tree.Root.Child = action;
-
                 tree.Tick();
 
                 action.Received().Update();
@@ -47,12 +135,6 @@ namespace Adnc.FluidBT.Testing {
 
             [Test]
             public void Tick_sets_root_as_current_on_action_status_success () {
-                var action = Substitute.For<INodeUpdate>();
-
-                action.Update().Returns(NodeStatus.Success);
-                action.Enabled.Returns(true);
-                tree.Root.Child = action;
-
                 tree.Tick();
 
                 Assert.AreEqual(tree.Current, tree.Root);
@@ -60,29 +142,21 @@ namespace Adnc.FluidBT.Testing {
 
             [Test]
             public void Tick_sets_root_as_current_on_status_failure () {
-                var action = Substitute.For<INodeUpdate>();
-
                 action.Update().Returns(NodeStatus.Failure);
-                action.Enabled.Returns(true);
-                tree.Root.Child = action;
 
                 tree.Tick();
 
                 Assert.AreEqual(tree.Current, tree.Root);
             }
 
-//            [Test]
-//            public void Tick_sets_action_as_current_on_status_continue () {
-//                var action = Substitute.For<INodeUpdate>();
-//
-//                action.Update().Returns(NodeStatus.Continue);
-//                action.Enabled.Returns(true);
-//                tree.Root.Child = action;
-//
-//                tree.Tick();
-//
-//                Assert.AreEqual(tree.Current, action);
-//            }
+            [Test]
+            public void Tick_sets_root_as_current_on_status_continue () {
+                action.Update().Returns(NodeStatus.Continue);
+
+                tree.Tick();
+
+                Assert.AreEqual(tree.Current, tree.Root);
+            }
         }
     }
 }
--- Assets/FluidBehaviorTree/Scripts/BehaviorTree/BehaviorTree.cs
+++ Assets/FluidBehaviorTree/Scripts/BehaviorTree/BehaviorTree.cs
@@ -1,18 +1,53 @@
-﻿using FluidBehaviorTree.Scripts.Nodes;
+﻿using System;
+using FluidBehaviorTree.Scripts.Nodes;
+using System.Collections.Generic;
 
 namespace Adnc.FluidBT.Trees {
     public class BehaviorTree {
-        public INodeRoot Root { get; set; } = new NodeRoot();
+        public INodeRoot Root { get; } = new NodeRoot();
 
         public INodeUpdate Current { get; set; }
 
+        public readonly HashSet<object> nodes = new HashSet<object>();
+
+        // @TODO Should only contain awake nodes
+        public readonly List<object> nodeList = new List<object>();
+
         public BehaviorTree () {
             Current = Root;
+            nodes.Add(Root);
+        }
+
+        public void AddNode (INodeChild parent, INodeUpdate child) {
+            if (parent == null) {
+                throw new ArgumentNullException(nameof(parent));
+            }
+
+            if (parent.Child != null) {
+                throw new ArgumentException("Cannot overwrite an already set child node");
+            }
+
+            if (child == null) {
+                throw new ArgumentNullException(nameof(child));
+            }
+
+            if (!nodes.Contains(parent)) {
+                throw new ArgumentException("Cannot add a node to a parent that is not in the BT");
+
+            }
+
+            if (nodes.Contains(child)) {
+                throw new ArgumentException("Cannot set a child node that has already been added");
+            }
+
+            parent.Child = child;
+            nodes.Add(child);
+            nodeList.Add(child);
         }
 
         public void Tick () {
             // Update must return the currently active node that should be reticked
-            // Two different Update methods
+            // @TODO Two different Update methods
                 // Tick: Returns the next node to be ticked
                 // Update: A status returned by a node (used by nodes that detect ticking)
             Current.Update();
