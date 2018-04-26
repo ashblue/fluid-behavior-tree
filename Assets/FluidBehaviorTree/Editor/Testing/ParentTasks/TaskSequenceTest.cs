debef025cee7917d613b668a7e10e45f31e37dc7
Charles Brindle <ash@blueashes.com>
2018-04-26 15:01:01 -0600

    Added basic sequence processing tests

--- Assets/FluidBehaviorTree/Editor/Testing/ParentTasks/TaskSequenceTest.cs
+++ Assets/FluidBehaviorTree/Editor/Testing/ParentTasks/TaskSequenceTest.cs
@@ -1,27 +1,54 @@
 ﻿using Adnc.FluidBT.TaskParents;
 using Adnc.FluidBT.Tasks;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Adnc.FluidBT.Testing {
     public class TaskSequenceTest {
-        public class TaskExample : TaskBase {
-        }
-
         public class UpdateMethod {
-            public void It_should_run_update_on_all_child_tasks () {
+            private ITask _childA;
+            private TaskSequence _sequence;
+            
+            [SetUp]
+            public void SetupChild () {
+                _sequence = new TaskSequence();
                 
-            }
-
-            public void It_should_return_success_if_all_child_tasks_pass () {
-                
+                _childA = Substitute.For<ITask>();
+                _childA.Update().Returns(TaskStatus.Success);
+                _sequence.AddChild(_childA);
             }
             
-            public void It_should_return_failure_if_a_child_task_fails () {
+            [Test]
+            public void It_should_run_update_on_all_child_tasks () {
+                var childB = Substitute.For<ITask>();
+                _sequence.AddChild(childB);
+                _sequence.Update();
                 
+                _sequence.children.ForEach((c) => c.Received().Update());
             }
 
-            public void It_should_return_continue_if_a_child_returns_continue () {
+            [Test]
+            public void It_should_return_success_if_all_child_tasks_pass () {
+                var childB = Substitute.For<ITask>();
+                _sequence.AddChild(childB);
+
+                _sequence.Update();
                 
+                Assert.AreEqual(TaskStatus.Success, _sequence.Update());
+            }
+            
+            [Test]
+            public void It_should_return_failure_if_a_child_task_fails () {
+                _childA.Update().Returns(TaskStatus.Failure);
+
+                Assert.AreEqual(TaskStatus.Failure, _sequence.Update());
+            }
+
+            [Test]
+            public void It_should_return_continue_if_a_child_returns_continue () {
+                _childA.Update().Returns(TaskStatus.Continue);
+
+                Assert.AreEqual(TaskStatus.Continue, _sequence.Update());
             }
         }
     }
--- Assets/FluidBehaviorTree/Scripts/TaskParents/Composites/Sequence/TaskSequence.cs
+++ Assets/FluidBehaviorTree/Scripts/TaskParents/Composites/Sequence/TaskSequence.cs
@@ -1,4 +1,16 @@
-﻿namespace Adnc.FluidBT.TaskParents {
+﻿using Adnc.FluidBT.Tasks;
+
+namespace Adnc.FluidBT.TaskParents {
     public class TaskSequence : TaskParentBase {
+        protected override TaskStatus OnUpdate () {
+            foreach (var child in children) {
+                var status = child.Update();
+                if (status != TaskStatus.Success) {
+                    return status;
+                }
+            }
+
+            return TaskStatus.Success;
+        }
     }
 }
