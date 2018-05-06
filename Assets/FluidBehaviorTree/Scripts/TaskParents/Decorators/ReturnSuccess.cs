57a97481a1a5dff07766a4bc8311531d165398b9
Ash Blue <ash@blueashes.com>
2018-05-06 16:50:07 -0600

    Added return failure and success decorators

---
+++ Assets/FluidBehaviorTree/Scripts/TaskParents/Decorators/ReturnSuccess.cs
@@ -0,0 +1,14 @@
+﻿using Adnc.FluidBT.Tasks;
+
+namespace Adnc.FluidBT.Decorators {
+    public class ReturnSuccess : DecoratorBase {
+        protected override TaskStatus OnUpdate () {
+            var status = child.Update();
+            if (status == TaskStatus.Continue) {
+                return status;
+            }
+
+            return TaskStatus.Success;
+        }
+    }
+}
