f429d914b824e3328d37856c090043b3e87a7fc4
Ash Blue <ash@blueashes.com>
2018-05-08 22:56:45 -0600

    Added generic decorator node

---
+++ Assets/FluidBehaviorTree/Editor/Testing/Decorators/DecoratorGenericTest.cs
@@ -0,0 +1,34 @@
+﻿using Adnc.FluidBT.Decorators;
+using Adnc.FluidBT.Tasks;
+using NUnit.Framework;
+
+namespace Adnc.FluidBT.Testing {
+    public class DecoratorGenericTest {
+        public class UpdateMethod {
+            [Test]
+            public void Can_invert_status_of_child () {
+                var task = new DecoratorGeneric {
+                    child = A.TaskStub().Build(),
+                    updateLogic = (dec) => {
+                        if (dec.child.Update() == TaskStatus.Success) {
+                            return TaskStatus.Failure;
+                        }
+
+                        return TaskStatus.Success;
+                    }
+                };
+
+                Assert.AreEqual(TaskStatus.Failure, task.Update());
+            }
+
+            [Test]
+            public void Returns_child_status_without_update_logic () {
+                var task = new DecoratorGeneric {
+                    child = A.TaskStub().Build()
+                };
+
+                Assert.AreEqual(TaskStatus.Success, task.Update());
+            }
+        }
+    }
+}
---
+++ Assets/FluidBehaviorTree/Scripts/TaskParents/Decorators/DecoratorGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Adnc.FluidBT.Tasks;
+
+namespace Adnc.FluidBT.Decorators {
+    public class DecoratorGeneric : DecoratorBase {
+        public Func<DecoratorBase, TaskStatus> updateLogic;
+
+        protected override TaskStatus OnUpdate () {
+            if (updateLogic != null) {
+                return updateLogic(this);
+            }
+
+            return child.Update();
+        }
+    }
+}
