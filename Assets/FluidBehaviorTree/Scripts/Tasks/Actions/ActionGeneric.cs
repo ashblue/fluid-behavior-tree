5e51dd04b3f9cdb6873a0924c936540ed34763c1
Ash Blue <ash@blueashes.com>
2018-05-08 22:21:44 -0600

    New generic action with hookable events

---
+++ Assets/FluidBehaviorTree/Scripts/Tasks/Actions/ActionGeneric.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Adnc.FluidBT.Tasks.Actions {
+    public class ActionGeneric : ActionBase {
+        public Func<TaskStatus> updateLogic;
+        public Action startLogic;
+        public Action initLogic;
+        public Action exitLogic;
+
+        protected override TaskStatus OnUpdate () {
+            if (updateLogic != null) {
+                return updateLogic();
+            }
+
+            return TaskStatus.Success;
+        }
+
+        protected override void OnStart () {
+            startLogic?.Invoke();
+        }
+
+        protected override void OnExit () {
+            initLogic?.Invoke();
+        }
+
+        protected override void OnInit () {
+            exitLogic?.Invoke();
+        }
+    }
+}
