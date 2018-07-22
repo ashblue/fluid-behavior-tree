7baaa892e9537ccce47bc3111856ab7094fa099e
Ash Blue <ash@blueashes.com>
2018-07-22 16:02:47 -0600

    Added basic CTF example

---
+++ Assets/FluidBehaviorTree/Examples/CaptureTheFlag/Scripts/FlagManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Adnc.FluidBT.Examples {
+    public class FlagManager : MonoBehaviour {
+        public static FlagManager current;
+        
+        public GameObject flag;
+        public GameObject goalRed;
+        public GameObject goalBlue;
+
+        [HideInInspector]
+        public GameObject flagStart;
+
+        private void Awake () {
+            flagStart = flag;
+            current = this;
+        }
+    }
+}
