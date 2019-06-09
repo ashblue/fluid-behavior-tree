466a07492cec6c55c72bf900999978536737851d
Ash Blue <ash@blueashes.com>
2019-06-08 22:19:40 -0600

    ci: final fix for nightly builds

--- publish-nightly.sh
+++ publish-nightly.sh
@@ -5,17 +5,10 @@ setup_git() {
     git config --global user.name "Travis CI - Bot"
 }
 
-commit_files() {
-    sed -i '/dist/d' ./.gitignore
-    git add dist
-    git commit -m "Update nightly files"
-}
-
 update_nightly_branch() {
     git remote add origin-nightly https://${GH_TOKEN}@github.com/ashblue/fluid-behavior-tree.git
-    git subtree push --prefix dist origin-nightly nightly
+    git subtree push --prefix Assets/FluidBehaviorTree origin-nightly nightly
 }
 
 setup_git
-commit_files
 update_nightly_branch
