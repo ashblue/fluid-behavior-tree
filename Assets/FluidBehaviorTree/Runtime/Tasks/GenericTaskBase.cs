7883dbaeb73a8a6ec15e9379ab408175c4ca7e98
Ash Blue <ash@blueashes.com>
2019-06-08 19:12:12 -0600

    refactor(visualizer): cleaned out test file

--- Assets/FluidBehaviorTree/Runtime/Tasks/GenericTaskBase.cs
+++ Assets/FluidBehaviorTree/Runtime/Tasks/GenericTaskBase.cs
@@ -4,7 +4,9 @@ namespace CleverCrow.Fluid.BTs.Tasks {
         protected const string PACKAGE_ROOT = "ROOT/Editor/Icons/Tasks";
 
         /// <summary>
-        /// For custom project icons provide a path from assets to a Texture2D asset. Example `Assets/MyIcon.png`
+        /// For custom project icons provide a path from assets to a Texture2D asset. Example `Assets/MyIcon.png`.
+        /// Be sure to NOT include the keyword `ROOT` in your path name. This will be replaced with a path
+        /// to the package root for Fluid Behavior Tree.
         /// </summary>
         public virtual string IconPath => $"{PACKAGE_ROOT}/Play.png";
         public virtual float IconPadding { get; }
