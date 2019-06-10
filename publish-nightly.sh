f697eec170553c85adb8f5f9e449d554eac1678c
Ash Blue <ash@blueashes.com>
2019-06-10 15:56:58 -0600

    revert(nightly): removing nightly back to what it originally was

--- publish-nightly.sh
+++ publish-nightly.sh
@@ -7,7 +7,7 @@ setup_git() {
 
 update_nightly_branch() {
     git remote add origin-nightly https://${GH_TOKEN}@github.com/ashblue/fluid-behavior-tree.git
-    git push origin-nightly `git subtree split --prefix Assets/FluidBehaviorTree master`:nightly --force
+    git subtree push --prefix Assets/FluidBehaviorTree origin-nightly nightly
 }
 
 setup_git
