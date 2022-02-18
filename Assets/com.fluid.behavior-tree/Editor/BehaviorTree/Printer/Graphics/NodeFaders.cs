a76bd66b8675c8714c4b41f81fb9bc97468f1a76
Elliot Drees <elliot@trellick.net>
2022-02-18 03:45:35 -0500

    feat: dark mode support
    Implemented the possibility to easily choose the box style.
    Made it so we can potentially change the box style later, and got rid of the borders to avoid calculating textures on the fly.

--- Assets/com.fluid.behavior-tree/Editor/BehaviorTree/Printer/Graphics/NodeFaders.cs
+++ Assets/com.fluid.behavior-tree/Editor/BehaviorTree/Printer/Graphics/NodeFaders.cs
@@ -1,15 +1,38 @@
 using UnityEngine;
+using UnityEditor;
 
 namespace CleverCrow.Fluid.BTs.Trees.Editors {
     public class NodeFaders {
-        public ColorFader BackgroundFader { get; } = new ColorFader(
-            new Color(0.65f, 0.65f, 0.65f), new Color(0.39f, 0.78f, 0.39f));
-        
-        public ColorFader TextFader { get; } = new ColorFader(
-            Color.white, Color.black);
-        
-        public ColorFader MainIconFader { get; } = new ColorFader(
-            new Color(1, 1, 1, 0.3f), new Color(1, 1, 1, 1f));
+        public ColorFader BackgroundFader { get; }
+
+        public ColorFader TextFader { get; }
+
+        public ColorFader MainIconFader { get; }
+
+        public NodeFaders() {
+            // If in dark mode choose dark mode
+            if (EditorGUIUtility.isProSkin){
+                BackgroundFader = new ColorFader(
+                    DisplaySettings.darkModeInactive,
+                    DisplaySettings.darkModeActive);
+                TextFader = new ColorFader(
+                    DisplaySettings.darkModeInactiveText,
+                    DisplaySettings.darkModeActiveText);
+                MainIconFader = new ColorFader(
+                    DisplaySettings.darkModeInactiveMainIcon,
+                    DisplaySettings.darkModeActiveMainIcon);
+            }else {
+                BackgroundFader = new ColorFader(
+                    DisplaySettings.lightModeInactive,
+                    DisplaySettings.lightModeActive);
+                TextFader = new ColorFader(
+                    DisplaySettings.lightModeInactiveText,
+                    DisplaySettings.lightModeActiveText);
+                MainIconFader = new ColorFader(
+                    DisplaySettings.lightModeInactiveMainIcon,
+                    DisplaySettings.lightModeActiveMainIcon);
+            }
+        }
 
         public void Update (bool active) {
             BackgroundFader.Update(active);
