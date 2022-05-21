254d33d6927710c7d07edb46de34879523f0bc4c
Yura Sokolov <migsil88@gmail.com>
2022-05-21 16:47:25 +0300

    feat: added Utils project with some default math extensions

---
+++ Assets/com.fluid.behavior-tree/Utils/MathFExtensions.cs
@@ -0,0 +1,22 @@
+namespace CleverCrow.Fluid.BTs.Trees.Utils
+{
+    public static class MathFExtensions
+    {
+        public const float Zero = 0f;
+
+        private const float HalfValue = 0.5f;
+        private const float TwiceValue = 2f;
+
+        public static float Half(this int original) => original * HalfValue;
+        public static float Half(this long original) => original * HalfValue;
+        public static float Half(this float original) => original * HalfValue;
+        public static float Half(this double original) => (float)(original * HalfValue);
+        public static float Half(this decimal original) => (float)original * HalfValue;
+
+        public static float Twice(this int original) => original * TwiceValue;
+        public static float Twice(this long original) => original * TwiceValue;
+        public static float Twice(this float original) => original * TwiceValue;
+        public static float Twice(this double original) => (float)(original * TwiceValue);
+        public static float Twice(this decimal original) => (float)original * TwiceValue;
+    }
+}
